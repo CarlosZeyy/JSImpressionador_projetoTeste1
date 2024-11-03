f88b895095a106770fc31b6d7ab95f684fc1c258
Carlos Moises <carloszeeyy@gmail.com>
2024-11-03 12:24:21 -0300

    contribuição de Carlos, o Dev

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o GitHub");
 turboLogger("Estou aprendendo o fluxo do git", "red");
-turboLogger(
-  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
-  "darkgreen"
-);
+turboLogger("Carlos, o dev, deixou sua contribuição nesse projeto!", "white");
