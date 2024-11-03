ee543c7a4092dbcf3068c36bf49823e31f1c2c87
Carlos Moises <carloszeeyy@gmail.com>
2024-11-03 12:08:09 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o GitHub");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
